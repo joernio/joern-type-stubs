112b11607a5120f649f222e9b445d47a70107294
Andrei Dreyer <andrei@whirlylabs.com>
2024-04-22 16:15:46 +0200

    scalafmt

--- schema/src/main/scala/CpgExtSchema.scala
+++ schema/src/main/scala/CpgExtSchema.scala
@@ -5,5 +5,5 @@ import overflowdb.schema.Property.ValueType
 object CpgSchema {
   val builder   = new SchemaBuilder(domainShortName = "Cpg", basePackage = "io.shiftleft.codepropertygraph.generated")
   val cpgSchema = new CpgSchema(builder)
-  val instance     = builder.build
+  val instance  = builder.build
 }
